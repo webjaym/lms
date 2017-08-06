a4a3aa77a3f4ceb95ef59cb3168b82262795fc1c
Jay <Jay>
2017-08-06 12:12:22 +0530

    LMS : first version commit

---
+++ src/com/web/plm/beans/Employee.java
@@ -0,0 +1,58 @@
+package com.web.plm.beans;
+
+public class Employee {
+	
+	private int empID;
+	private String firstName;
+	private String lastName;
+	private String designation;
+	private String emailID;
+	private int mobNo;
+	private boolean isActive;
+	
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public int getMobNo() {
+		return mobNo;
+	}
+	public void setMobNo(int mobNo) {
+		this.mobNo = mobNo;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+
+}
